103cc238b0c1784a1b82ddd5172c31c32d65a62c
Volodymyr Vysotskyi <vvvladimir65@gmail.com>
2024-06-14 15:45:22 +0200

    Train

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ n_seats = 4
 df = pd.read_excel('./utils/Example Excel Template.xlsx')
 
 colleagues_list = df['Colleagues'].tolist()
-
+print(colleagues_list)
 a = Openspace(n_tables,n_seats)
 print(a.organize(colleagues_list))
 print(a.display())
